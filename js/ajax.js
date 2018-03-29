0adb60ce9520c207be45da4376e06744ebf111a9
fatetianchen <398272208@qq.com>
2018-03-29 18:51:52 +0800

    first commit

---
+++ js/ajax.js
@@ -0,0 +1,18 @@
+$.ajax({
+    url: 'http://192.168.1.28:3000/api/users',
+    method:'GET',
+    dataType: "json",
+    data:{},//false true
+    success: function(data){
+        console.log(data.length);
+        for(var i in data) {
+                 $('.ranking-ajax').append('<div class="clearfix"><div class="rk-number fl"></div><div class="rk-x fl"></div><div class="rk-name fl">'+data[i].nickname+'</div><div class="rk-integral fr clearfix"><i></i><span>'+data[i].scoreRecords[0].score+'</span></div></div>')    
+                       
+        }
+        
+    },					
+    error : function(err) {
+        console.log(err);
+    }
+    });
+    //<img src='+data[i].headimg+' alt=""> 
